01000d33c0e836ee39393deedf1ce043fcacd010
Nathachai Thongniran <inid3a@gmail.com>
2016-11-09 15:23:32 +0700

    Update dist files

--- dist/js/fastest-slider.js
+++ dist/js/fastest-slider.js
@@ -1,242 +1,3 @@
-/*
- * classList.js: Cross-browser full element.classList implementation.
- * 2014-12-13
- *
- * By Eli Grey, http://eligrey.com
- * Public Domain.
- * NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
- */
-
-/*global self, document, DOMException */
-
-/*! @source http://purl.eligrey.com/github/classList.js/blob/master/classList.js */
-
-if ("document" in self) {
-
-// Full polyfill for browsers with no classList support
-if (!("classList" in document.createElement("_"))) {
-
-(function (view) {
-
-"use strict";
-
-if (!('Element' in view)) return;
-
-var
-	  classListProp = "classList"
-	, protoProp = "prototype"
-	, elemCtrProto = view.Element[protoProp]
-	, objCtr = Object
-	, strTrim = String[protoProp].trim || function () {
-		return this.replace(/^\s+|\s+$/g, "");
-	}
-	, arrIndexOf = Array[protoProp].indexOf || function (item) {
-		var
-			  i = 0
-			, len = this.length
-		;
-		for (; i < len; i++) {
-			if (i in this && this[i] === item) {
-				return i;
-			}
-		}
-		return -1;
-	}
-	// Vendors: please allow content code to instantiate DOMExceptions
-	, DOMEx = function (type, message) {
-		this.name = type;
-		this.code = DOMException[type];
-		this.message = message;
-	}
-	, checkTokenAndGetIndex = function (classList, token) {
-		if (token === "") {
-			throw new DOMEx(
-				  "SYNTAX_ERR"
-				, "An invalid or illegal string was specified"
-			);
-		}
-		if (/\s/.test(token)) {
-			throw new DOMEx(
-				  "INVALID_CHARACTER_ERR"
-				, "String contains an invalid character"
-			);
-		}
-		return arrIndexOf.call(classList, token);
-	}
-	, ClassList = function (elem) {
-		var
-			  trimmedClasses = strTrim.call(elem.getAttribute("class") || "")
-			, classes = trimmedClasses ? trimmedClasses.split(/\s+/) : []
-			, i = 0
-			, len = classes.length
-		;
-		for (; i < len; i++) {
-			this.push(classes[i]);
-		}
-		this._updateClassName = function () {
-			elem.setAttribute("class", this.toString());
-		};
-	}
-	, classListProto = ClassList[protoProp] = []
-	, classListGetter = function () {
-		return new ClassList(this);
-	}
-;
-// Most DOMException implementations don't allow calling DOMException's toString()
-// on non-DOMExceptions. Error's toString() is sufficient here.
-DOMEx[protoProp] = Error[protoProp];
-classListProto.item = function (i) {
-	return this[i] || null;
-};
-classListProto.contains = function (token) {
-	token += "";
-	return checkTokenAndGetIndex(this, token) !== -1;
-};
-classListProto.add = function () {
-	var
-		  tokens = arguments
-		, i = 0
-		, l = tokens.length
-		, token
-		, updated = false
-	;
-	do {
-		token = tokens[i] + "";
-		if (checkTokenAndGetIndex(this, token) === -1) {
-			this.push(token);
-			updated = true;
-		}
-	}
-	while (++i < l);
-
-	if (updated) {
-		this._updateClassName();
-	}
-};
-classListProto.remove = function () {
-	var
-		  tokens = arguments
-		, i = 0
-		, l = tokens.length
-		, token
-		, updated = false
-		, index
-	;
-	do {
-		token = tokens[i] + "";
-		index = checkTokenAndGetIndex(this, token);
-		while (index !== -1) {
-			this.splice(index, 1);
-			updated = true;
-			index = checkTokenAndGetIndex(this, token);
-		}
-	}
-	while (++i < l);
-
-	if (updated) {
-		this._updateClassName();
-	}
-};
-classListProto.toggle = function (token, force) {
-	token += "";
-
-	var
-		  result = this.contains(token)
-		, method = result ?
-			force !== true && "remove"
-		:
-			force !== false && "add"
-	;
-
-	if (method) {
-		this[method](token);
-	}
-
-	if (force === true || force === false) {
-		return force;
-	} else {
-		return !result;
-	}
-};
-classListProto.toString = function () {
-	return this.join(" ");
-};
-
-if (objCtr.defineProperty) {
-	var classListPropDesc = {
-		  get: classListGetter
-		, enumerable: true
-		, configurable: true
-	};
-	try {
-		objCtr.defineProperty(elemCtrProto, classListProp, classListPropDesc);
-	} catch (ex) { // IE 8 doesn't support enumerable:true
-		if (ex.number === -0x7FF5EC54) {
-			classListPropDesc.enumerable = false;
-			objCtr.defineProperty(elemCtrProto, classListProp, classListPropDesc);
-		}
-	}
-} else if (objCtr[protoProp].__defineGetter__) {
-	elemCtrProto.__defineGetter__(classListProp, classListGetter);
-}
-
-}(self));
-
-} else {
-// There is full or partial native classList support, so just check if we need
-// to normalize the add/remove and toggle APIs.
-
-(function () {
-	"use strict";
-
-	var testElement = document.createElement("_");
-
-	testElement.classList.add("c1", "c2");
-
-	// Polyfill for IE 10/11 and Firefox <26, where classList.add and
-	// classList.remove exist but support only one argument at a time.
-	if (!testElement.classList.contains("c2")) {
-		var createMethod = function(method) {
-			var original = DOMTokenList.prototype[method];
-
-			DOMTokenList.prototype[method] = function(token) {
-				var i, len = arguments.length;
-
-				for (i = 0; i < len; i++) {
-					token = arguments[i];
-					original.call(this, token);
-				}
-			};
-		};
-		createMethod('add');
-		createMethod('remove');
-	}
-
-	testElement.classList.toggle("c3", false);
-
-	// Polyfill for IE 10 and Firefox <24, where classList.toggle does not
-	// support the second argument.
-	if (testElement.classList.contains("c3")) {
-		var _toggle = DOMTokenList.prototype.toggle;
-
-		DOMTokenList.prototype.toggle = function(token, force) {
-			if (1 in arguments && !this.contains(token) === !force) {
-				return force;
-			} else {
-				return _toggle.call(this, token);
-			}
-		};
-
-	}
-
-	testElement = null;
-}());
-
-}
-
-}
-
-
 // prefix: fasl
 
 var FastestSlider = function(option) {
@@ -274,38 +35,93 @@ var FastestSlider = function(option) {
   /*================================================================ Util
    */
   
+  /**
+   * Returns ture if browser if IE
+   * 
+   * @return {boolean}
+   */
   this.isIe = function() {
     var myNav = navigator.userAgent.toLowerCase();
 
     return (myNav.indexOf('msie') !== -1) ? parseInt(myNav.split('msie')[1]) : false;
   };
 
-  // https://developer.mozilla.org/en/docs/Web/API/Element/setAttribute
+  /**
+   * Set element's data attribute
+   *
+   * @see https://developer.mozilla.org/en/docs/Web/API/Element/setAttribute
+   * 
+   * @param {Object} ele
+   * @param {string} attrName
+   * @param {string} value
+   * 
+   * @return {Object} ele
+   */
   this.setAttr = function(ele, attrName, value) {
     return ele.setAttribute('data-' + this.prefix + '-' + attrName, value);
   };
 
+  /**
+   * Get element's data attribute
+   *
+   * @see https://developer.mozilla.org/en/docs/Web/API/Element/setAttribute
+   * 
+   * @param {Object} ele
+   * @param {string} attrName
+   * 
+   * @return {Object} ele
+   */
   this.getAttr = function(ele, attrName) {
     return ele.getAttribute('data-' + this.prefix + '-' + attrName);
+  };
+
+  /**
+   * Check number is in a range ?
+   * 
+   * @see http://stackoverflow.com/questions/12806304/shortest-code-to-check-if-a-number-is-in-a-range-in-javascript
+   * @see http://stackoverflow.com/questions/6454198/check-a-range-of-numbers-in-an-if-condition
+   * 
+   * @param {number} x
+   * @param {number} min
+   * @param {number} max
+   * @return {boolean}
+   */
+  this.isBetween = function(x, min, max) {
+    return x >= min && x <= max;
   };
 
   /*================================================================ Plugin util
    */
   
+  /**
+   * Show element
+   * remove css class that hide the element
+   * 
+   * @param {Object} ele
+   * @return {Object} ele
+   */
   this.showElement = function(ele) {
     ele.classList.remove('fasl-hide');
-    ele.classList.add('fasl-show');
 
     return ele;
   };
 
+  /**
+   * Hide element
+   * add css class that hide the element
+   * 
+   * @param {Object} ele
+   * @return {Object} ele
+   */
   this.hideElement = function(ele) {
-    ele.classList.remove('fasl-show');
     ele.classList.add('fasl-hide');
 
     return ele;
   };
 
+  /**
+   * Initialize prev arrow 
+   */
   this.initPrevArrow = function() {
     var self = this;
 
@@ -325,6 +141,9 @@ var FastestSlider = function(option) {
     }
   };
 
+  /**
+   * Initialize next arrow
+   */
   this.initNextArrow = function() {
     var self = this;
 
@@ -344,6 +163,9 @@ var FastestSlider = function(option) {
     }
   };
 
+  /**
+   * Initialize slider
+   */
   this.initSlider = function() {
     this.items = this.group.children;
 
@@ -377,14 +199,31 @@ var FastestSlider = function(option) {
     }
   };
 
+  /**
+   * Log
+   */
   this.log = function(title, data) {
     if (typeof data === 'undefined') { data = ''; }
     if (this.option.isDebug) { console.log(this.option.selector + ' ' + title, data); }
   };
 
+  /**
+   * Check the goToIndex is valid or not
+   * 
+   * @param {number} index - integer number
+   * @return {boolean}
+   */
+  this.isValidGoToIndex = function(index) {
+    return this.isBetween(index, 0, this.items.length);
+  };
+
   /*================================================================ API
    */
   
+  /**
+   * Set interval
+   * - auto slide
+   */
   this.startInterval = function(ms) {
     if (typeof ms === 'undefined') { ms = this.option.slideSpeed; }
     var self = this;
@@ -396,14 +235,64 @@ var FastestSlider = function(option) {
     }, ms);
   };
 
+  /**
+   * Stop interval
+   */
   this.stopInterval = function() {
     clearInterval(this.interval);
   };
 
-  /*================================================================ Custom event
+  /**
+   * Slide to next slide
+   * caller
    */
+  this.goToNext = function() {
+    this.slideToNextItem();
+  };
 
-  // TODO: refactor, eventName validatiion
+  /**
+   * Slide to prev slide
+   * caller
+   */
+  this.goToPrev = function() {
+    this.slideToPrevItem();
+  };
+
+  /**
+   * Slide to slide by slide's index
+   * caller 
+   * 
+   * @param {string|number} goToIndex
+   */
+  this.goTo = function(goToIndex) {
+    if (goToIndex === '-1') {
+      this.slideToPrevItem();
+
+    } else if (goToIndex === '+1') {
+      this.slideToNextItem();
+      
+    } else {
+      goToIndex = parseInt(goToIndex);
+
+      if (this.isValidGoToIndex(goToIndex)) {
+        this.slideTo(goToIndex);
+
+      } else {
+        this.log('invalid goToIndex')
+      }
+    }
+  };
+
+  /*================================================================ API - Custom event
+   */
+  
+  /**
+   * Adding callback event
+   * TODO: refactor, eventName validatiion
+   * 
+   * @param {string} eventName
+   * @param {function} callback
+   */
   this.on = function(eventName, callback) {
     if (typeof callback === 'function') {
       switch (eventName) {
@@ -419,14 +308,25 @@ var FastestSlider = function(option) {
     }
   };
 
+  /**
+   * Release beforeChange event
+   */
   this.releaseBeforeChangeEvent = function() {
     this.releaseEvent('beforeChange');
   };
 
+  /**
+   * Release afterChange event
+   */
   this.releaseAfterChangeEvent = function() {
     this.releaseEvent('afterChange');
   };
 
+  /**
+   * Release event by event name
+   * 
+   * @param {string} eventName - plugin's event name
+   */
   this.releaseEvent = function(eventName) {
     if (this.callback[eventName]) {
       var nEvents = this.callback[eventName].length,
@@ -441,33 +341,50 @@ var FastestSlider = function(option) {
   /*================================================================ Others
    */
   
+  /**
+   * Get prev-slide's index
+   */
   this.getPrevItemIndex = function() {
     var result = (this.currentItemIndex <= 0) ? this.items.length - 1 : this.currentItemIndex - 1;
 
     return result;
   };
   
+  /**
+   * Get next-slide's index
+   */
   this.getNextItemIndex = function() {
     var result = (this.currentItemIndex >= this.items.length - 1) ? 0 : this.currentItemIndex + 1;
 
     return result;
   };
   
+  /**
+   * Slide to next slide
+   */
   this.slideToNextItem = function() {
-    var currentIndex = this.currentItemIndex;
     var nextIndex = this.getNextItemIndex();
 
-    this.slideItem(currentIndex, nextIndex);
+    this.slideTo(nextIndex);
   };
 
+  /**
+   * Slide to prev slide
+   */
   this.slideToPrevItem = function() {
-    var currentIndex = this.currentItemIndex;
     var prevIndex = this.getPrevItemIndex();
 
-    this.slideItem(currentIndex, prevIndex);
+    this.slideTo(prevIndex);
   };
 
-  this.slideItem = function(currentIndex, slideToIndex) {
+  /**
+   * Slide to slide by slide's index 
+   * 
+   * @param {number} slideToIndex
+   */
+  this.slideTo = function(slideToIndex) {
+    var currentIndex = this.currentItemIndex;
+
     // dispatch
     this.releaseBeforeChangeEvent();
     
@@ -479,7 +396,11 @@ var FastestSlider = function(option) {
     this.releaseAfterChangeEvent();
   };
 
-  // validate data, and initialize it 
+  /**
+   * Initialize plugin
+   * - validate data
+   * - initialize
+   */
   this.init = function() {
     // this.option = Object.assign(this.defaultOption, this.userOption);
 
